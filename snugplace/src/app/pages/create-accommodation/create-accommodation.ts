e4eb6392d8783642cf7da6825ca9cba36bdb6e59
Wyllinton <willinton.vergarac@uqvirtual.edu.co>
2025-10-26 23:22:58 -0500

    Layout for createAccommodation and profile forms

--- snugplace/src/app/pages/create-accommodation/create-accommodation.ts
+++ snugplace/src/app/pages/create-accommodation/create-accommodation.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { AbstractControlOptions, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { repeat } from 'rxjs';
 
 @Component({
   selector: 'app-create-accommodation',
@@ -8,5 +9,60 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './create-accommodation.css'
 })
 export class CreateAccommodation {
+
+  createAccommodationForm!: FormGroup;
+  selectedImages: File[] = [];
+  cities: string[];
+  servicesList: string[];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.createForm();
+    this.cities = ['Bogotá', 'Medellín', 'Cali','Barranquilla','Cartagena','Bucaramanga','Pereira','Manizales','Armenia','Santa Marta','Ibagué','Neiva','Pasto'];
+    this.servicesList = ['WiFi', 'Desayuno incluido', 'Piscina', 'Aire acondicionado', 'Estacionamiento gratuito', 'Gimnasio', 'Transporte al aeropuerto', 'Admite mascotas', 'Servicio de limpieza', 'TV por cable'];
+  }
+
+  private createForm() {
+    this.createAccommodationForm = this.formBuilder.group({
+    title: ['', [Validators.required]],
+    description: ['', [Validators.required, Validators.maxLength(500)]],
+    city: ['', [Validators.required]],
+    address: ['', [Validators.required]],
+    location: ['', [Validators.required]],
+    priceDay: [0, [Validators.required, Validators.pattern(/^[0-9]+$/),Validators.min(1)]],
+    guestsCount: [1, [Validators.required, Validators.pattern(/^[0-9]+$/), Validators.min(1)]],
+    services: ['', [Validators.required]],
+    images: [[], [Validators.required]]
+  }
+  );
+  }
+
+  public createAccommodation(){
+    console.log(this.createAccommodationForm.value);
+  }
+
+  public onFileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    
+    if (input.files && input.files.length > 0) {
+      const files = Array.from(input.files);
+      this.createAccommodationForm.patchValue({ images: files });
+    }
+  }
+
+  onServiceChange(event: any) {
+    const selectedServices = this.createAccommodationForm.value.services || [];
+    const value = event.target.value;
+
+    if (event.target.checked) {
+      selectedServices.push(value);
+    } else {
+      const index = selectedServices.indexOf(value);
+      if (index !== -1) {
+        selectedServices.splice(index, 1);
+      }
+    }
+
+    this.createAccommodationForm.patchValue({ services: selectedServices });
+  }
 
 }
--- snugplace/src/app/pages/profile/profile.ts
+++ snugplace/src/app/pages/profile/profile.ts
@@ -1,11 +1,59 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [ReactiveFormsModule ],
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
 export class Profile {
+  profileForm!: FormGroup;
 
+   // Data for testing purposes
+  currentUser = {
+    name: 'Juan Pérez',
+    phoneNumber: '3104567890',
+    description: 'Anfitrión apasionado por el turismo ecológico',
+    email: 'juanperez@example.com'
+  };
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+    this.loadUserData();
+  }
+
+
+  private createForm() {
+    this.profileForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      phoneNumber: ['', [Validators.required, Validators.maxLength(10)]],
+      description: ['', [Validators.maxLength(200)]],
+      email: [{ value: '', disabled: true }] 
+    });
+  }
+
+  /** Load de data´s user authenticaded in the form */
+  private loadUserData() {
+    this.profileForm.patchValue(this.currentUser);
+  }
+
+  updateProfile() {
+    if (this.profileForm.valid) {
+      const updatedProfile = this.profileForm.getRawValue();
+      console.log('Perfil actualizado:', updatedProfile);
+
+      // Aquí puedes llamar a tu servicio para actualizar en el backend:
+      // this.userService.updateProfile(updatedProfile).subscribe(...)
+    } else {
+      console.warn('Formulario inválido. Por favor verifique los campos.');
+      this.profileForm.markAllAsTouched();
+    }
+  }
+
+  isInvalidField(field: string): boolean {
+    const control = this.profileForm.get(field);
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
 }
